added01b34b92ccaef02755427d068293099625b
hazkura36 <59018783+hazkura36@users.noreply.github.com>
2019-12-19 09:58:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,6 +20,14 @@ async def on_ready():
     print('ready')
 
 
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')  
+
+#ループ処理実行
+loop.start()
         
 @bot.command()
 async def ping(ctx):
